909ca027efea2b602c809ab1f921e212d495e930
daniLuque <daniluquecastro@gmail.com>
2019-10-27 13:33:34 +0100

    String hecho

--- Tema3/strings.js
+++ Tema3/strings.js
@@ -7,17 +7,30 @@
 
 //Por ejemplo: obtenerNuevaFrase("Espero ir al cine a ver el joker","e") => "ver joker";
 //Por ejemplo: obtenerNuevaFrase("Todo depende de si el raton si acaba pronto con el queso","to") => "raton";
-function obtenerNuevaFrase(frase,trozoPalabra){
+function obtenerNuevaFrase(frase, trozoPalabra) {
 
-    
+
 }
 
 //Función que devuelve la suma de todas las posiciones
 //En la que se encuentra una palabra en una frase.
 //Por ejemplo: sumaDePosiciones("Prueba de la rueda","ue") => 16
 //Por ejemplo: sumaDePosiciones("Aclaremos el ejercicio","acl") => 0
-function sumaDePosiciones(frase,trozoPalabra){
+function sumaDePosiciones(frase, trozoPalabra) {
+    let fraseCom = frase.split(" ");
+    let suma = 0;
+    let sumaTotal = 0;
+    for (let i = 0; fraseCom.length > i; i++) {
+        if (fraseCom[i].includes(trozoPalabra)) {
+            suma = frase.indexOf(trozoPalabra, suma + 1);
+            if (suma === -1) {
+                suma = 0;
+            }
+            sumaTotal = sumaTotal + suma;
 
+        }
+    }
+    return sumaTotal;
 }
 
 //Función que valida el formato RGB de los colores
@@ -26,8 +39,28 @@ function sumaDePosiciones(frase,trozoPalabra){
 //Los ejemplos de colores RGB son:
 //     #123ABC o #123abc
 //     #BBB    o #333     
-function esValidoFormatoRGB(color){
-
+function esValidoFormatoRGB(color) {
+    colorMinusculo = color.toLocaleLowerCase();
+    let result = false;
+    let coloresHexa = ["a", "b", "c", "d", "e", "f", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    let cont = 0;
+    if (color.startsWith("#")) {
+        for (let j = 1; color.length > j; j++) {
+            let comparar = colorMinusculo.charAt(j);
+            for (let i = 0; coloresHexa.length > i; i++) {
+                if (comparar.includes(coloresHexa[i]) === true) {
+                    cont++;
+                }
+            }
+        }
+    }
+    if (cont === color.length - 1) {
+        result = true;
+    }
+    if (colorMinusculo.endsWith("#")) {
+        result = false;
+    }
+    return result;
 }
 
 //Función que valida una URL de una web .com y .es
@@ -39,6 +72,21 @@ function esValidoFormatoRGB(color){
 //Por ejemplo: http://www.prueba.es?ejemplo=1 =>no valido
 //Por ejemplo: http://www.prueba.es => No valido
 //Por ejemplo: https://www.prueba?hola=1&holita=2&holar=3 =>No valido
-function esValidaURL(color){
+function esValidaURL(color) {
+    resul = false;
+    urlSeparada = url.split(".");
+    if (urlSeparada.length > 2) {
 
+        if (url.startsWith("http://www.") || url.startsWith("https://www.")) {
+            if (urlSeparada[2].startsWith("com") || urlSeparada[2].startsWith("es")) {
+
+                if (urlSeparada[2].includes("&") === true) {
+
+                    resul = true;
+
+                }
+            }
+        }
+    }
+    return resul;
 }
